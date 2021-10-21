27c191fa71faafd12f83a95e33a435186f65f882
AishwaryaBhalchandraYadav <aishuyadav230501@gmail.com>
2021-10-21 19:15:16 +0530

    third initial commit

--- frontend/src/component/layout/About/About.js
+++ frontend/src/component/layout/About/About.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./aboutSection.css";
-import { Button, Typography, Avatar } from "@material-ui/core";
-import YouTubeIcon from "@material-ui/icons/YouTube";
-import InstagramIcon from "@material-ui/icons/Instagram";
+
+
+import {  Typography, } from "@material-ui/core";
+//  import YouTubeIcon from "@material-ui/icons/YouTube";
+// import InstagramIcon from "@material-ui/icons/Instagram";
 const About = () => {
-  const visitInstagram = () => {
-    window.location = "https://instagram.com/meabhisingh";
-  };
+  // const visitInstagram = () => {
+  // //  window.location = "https://instagram.com/meabhisingh";//
+  // };
+
   return (
+
+    
     <div className="aboutSection">
       <div></div>
       <div className="aboutSectionGradient"></div>
@@ -16,37 +21,77 @@ const About = () => {
 
         <div>
           <div>
-            <Avatar
+            {/* <Avatar
               style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
               src="https://res.cloudinary.com/tripleayt/image/upload/v1631555947/products/jpyibarlaxawvcvqjv5b.png"
               alt="Founder"
-            />
-            <Typography>Abhishek Singh</Typography>
+            /> */}
+            {/* <Typography>Abhishek Singh</Typography>
             <Button onClick={visitInstagram} color="primary">
               Visit Instagram
-            </Button>
+            </Button> */}
             <span>
-              This is a sample wesbite made by @meabhisingh. Only with the
-              purpose to teach MERN Stack on the channel 6 Pack Programmer
+          <h2>  Welcome </h2>
+    <br/>
+consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
             </span>
           </div>
           <div className="aboutSectionContainer2">
-            <Typography component="h2">Our Brands</Typography>
-            <a
-              href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
+            {/* <Typography component="h2">Our Brands</Typography> */}
+           
+            <img src="https://p.w3layouts.com/demos_new/template_demo/28-08-2018/electro_store-demo_Free/1204782700/web/images/ab.jpg" class="img fluid"/>
+           
+            {/* <a
+            //   href="https://www.youtube.com/channel/UCO7afj9AUo0zV69pqEYhcjw"
               target="blank"
             >
-              <YouTubeIcon className="youtubeSvgIcon" />
-            </a>
+              <YouTubeIcon className="youtubeSvgIcon" /> */}
+             
 
-            <a href="https://instagram.com/meabhisingh" target="blank">
+            {/* <a href="https://instagram.com/meabhisingh" target="blank">
               <InstagramIcon className="instagramSvgIcon" />
-            </a>
+            </a>  */}
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  
 
+//     <div class="about">
+//         <div class="container">
+//             <div class="row">
+//                 <div class="col-xl-5 col-lg-5 col-md-5 co-sm-l2">
+//                     <div>
+//                     <div class="about_img">
+//                         <figure><img src= {about}alt="img" /></figure>
+//                     </div>
+//                 </div>
+//                 <div class="col-xl-7 col-lg-7 col-md-7 co-sm-l2">
+//                     <div class="about_box">
+//                         <span>Our Mobile Shop</span>
+//                         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of It is a long established fact that a reader will be distracted by the </p>
+
+//                     </div>
+//                     </div>
+//                 </div>
+
+//                 <div class="col-xl-7 col-lg-7 col-md-7 co-sm-l2">
+//                     <div class="about_box_ ">
+//                         <span>Our Mobile Shop</span>
+//                         <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of It is a long established fact that a reader will be distracted by the </p>
+
+//                     </div>
+//                 </div>
+//                 <div class="col-xl-5 col-lg-5 col-md-5 co-sm-l2">
+//                     <div class="about_img">
+//                         <figure><img src={about} alt="img" /></figure>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+    
+    
+  );
+  };  
 export default About;
